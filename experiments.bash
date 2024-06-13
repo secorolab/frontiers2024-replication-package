c7aef26dc0f73fcfb68dd2d95e4bdec0f1377313
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2024-06-13 13:42:22 +0200

    Rename icra level 3b world to brsu dynamic

--- experiments.bash
+++ experiments.bash
@@ -6,7 +6,7 @@ SCENARIO_CONFIG="fro"
 for i in {1..5}; do roslaunch --log floorplan-dsl-environments frontiers_scenarios.launch scenario:=scenario1 environment_name:=brsu_building_c_static init_pos_x:=0.0 init_pos_y:=0.0 route_file:=left_long_corridor_task map_name:=brsu_building_c_static robot_config:=$SCENARIO_CONFIG; done
 
 # Scenario 2
-for i in {1..5}; do roslaunch --log  floorplan-dsl-environments frontiers_scenarios.launch scenario:=scenario2 environment_name:=icra_brsu_building_c_level_3b init_pos_x:=0.0 init_pos_y:=0.0 route_file:=left_long_corridor_task map_name:=brsu_building_c_static robot_config:=$SCENARIO_CONFIG; done
+for i in {1..5}; do roslaunch --log  floorplan-dsl-environments frontiers_scenarios.launch scenario:=scenario2 environment_name:=brsu_building_c_dynamic init_pos_x:=0.0 init_pos_y:=0.0 route_file:=left_long_corridor_task map_name:=brsu_building_c_static robot_config:=$SCENARIO_CONFIG; done
 
 # Scenario 3
 for i in {1..5}; do roslaunch --log floorplan-dsl-environments frontiers_scenarios.launch scenario:=scenario3.1 environment_name:=frontiers_brsu_building_c_adversarial route_file:=task_1 map_name:=brsu_building_c_dynamic robot_config:=$SCENARIO_CONFIG; done
